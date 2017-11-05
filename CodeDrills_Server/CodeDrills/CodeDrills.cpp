10ec440b99ef88646567dd89c7e6d3ce449c8ba4
Tetris566 <thelastloner@gmail.com>
2017-11-04 22:18:07 -0700

    Initial commit
    Pre existing project files being commited on first day of repo use.

---
+++ CodeDrills_Server/CodeDrills/CodeDrills.cpp
@@ -0,0 +1,141 @@
+// CodeDrills.cpp : Defines the entry point for the console application.
+//
+// This is just for practicing.
+
+#include "stdafx.h"
+#include <winsock2.h>
+#include <iostream>
+#include <string>
+#include <ws2tcpip.h>
+#include <stdio.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <signal.h>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+#define PORT "3490"    // the port users will be connecting to
+
+#define BACKLOG 10     // how many pending connections queue will hold
+
+void sigchld_handler(int s)
+{
+	// waitpid() might overwrite errno, so we save and restore it:
+	int saved_errno = errno;
+
+	//while (waitpid(-1, NULL, WNOHANG) > 0);
+
+	errno = saved_errno;
+}
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+	if (sa->sa_family == AF_INET) {
+		return &(((struct sockaddr_in*)sa)->sin_addr);
+	}
+
+	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+int main()
+{
+#pragma region WinSock Startup
+
+	WSADATA wsaData;
+
+	int iResult;
+
+	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);					// This starts Winsock. It needs to be run early.
+	if (iResult != 0) {
+		printf("WSAStartup failed: %d\n", iResult);
+		return 1;
+	}
+
+#pragma endregion
+
+	int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
+	struct addrinfo hints, *servinfo, *p;
+	struct sockaddr_storage their_addr; // connector's address information
+	socklen_t sin_size;
+	//struct sigaction sa;
+	int yes = 1;
+	char s[INET6_ADDRSTRLEN];
+	int rv;
+
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE; // use my IP
+
+	if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		return 1;
+	}
+
+	// loop through all the results and bind to the first we can
+	for (p = servinfo; p != NULL; p = p->ai_next) {
+		if ((sockfd = socket(p->ai_family, p->ai_socktype,
+			p->ai_protocol)) == -1) {
+			perror("server: socket");
+			continue;
+		}
+
+		if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, "1",
+			sizeof(int)) == -1) {
+			perror("setsockopt");
+			exit(1);
+		}
+
+		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+			closesocket(sockfd);
+			perror("server: bind");
+			continue;
+		}
+
+		break;
+	}
+
+	freeaddrinfo(servinfo); // all done with this structure
+
+	if (p == NULL) {
+		fprintf(stderr, "server: failed to bind\n");
+		exit(1);
+	}
+
+	if (listen(sockfd, BACKLOG) == -1) {
+		perror("listen");
+		exit(1);
+	}
+
+	printf("server: waiting for connections...\n");
+
+	while (1) {  // main accept() loop
+		sin_size = sizeof their_addr;
+		new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+		if (new_fd == -1) {
+			perror("accept");
+			continue;
+		}
+
+		inet_ntop(their_addr.ss_family,
+			get_in_addr((struct sockaddr *)&their_addr),
+			s, sizeof s);
+		printf("server: got connection from %s\n", s);
+
+		closesocket(sockfd); // child doesn't need the listener
+
+		std::string input;
+		getline(std::cin, input);
+
+		if (send(new_fd, input.c_str() , 13, 0) == -1)perror("send");
+		closesocket(new_fd);
+		exit(0);
+	}
+
+	return 0;
+
+	WSACleanup();													// Run this when sockets are done being used.
+	system("pause");
+}
+
